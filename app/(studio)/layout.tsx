921802a1ff7afbbf90011ed07e692c2b326c4115
brianlau127 <brianlau2002@gmail.com>
2025-06-23 18:19:17 +0800

    Update layout.tsx

--- app/(studio)/layout.tsx
+++ app/(studio)/layout.tsx
@@ -14,13 +14,13 @@
 
 'use client'
 
-import SideNav from '../ui/transverse-components/SideNavigation'
+import TopNav from '../ui/transverse-components/SideNavigation'
 import Box from '@mui/material/Box'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Box sx={{ display: 'flex' }}>
-      <SideNav />
+      <TopNav />
       {children}
     </Box>
   )
