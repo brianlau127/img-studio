a4f69eac26b1ae49fc0c0576297982086a59f415
brianlau127 <brianlau2002@gmail.com>
2025-06-24 11:58:44 +0800

    Update generate-image-utils.tsx

--- app/api/generate-image-utils.tsx
+++ app/api/generate-image-utils.tsx
@@ -117,13 +117,7 @@ export const GenerateImageFormFields = {
     label: 'Aspect ratio',
     type: 'chip-group',
     default: '1:1',
-    options: ['1:1', '9:16', '16:9', '3:4', '4:3','6:5',
-    '4:15',
-    '1:2',
-    '728:90',
-    '970:90',
-    '970:250',
-    '320:100'],
+    options: ['1:1', '9:16', '16:9', '3:4', '4:3'],
     isDataResetable: false,
     isFullPromptAdditionalField: false,
   },
@@ -528,14 +522,7 @@ export const RatioToPixel = [
   { ratio: '9:16', width: 768, height: 1408 },
   { ratio: '16:9', width: 1408, height: 768 },
   { ratio: '3:4', width: 896, height: 1280 },
-  { ratio: '4:3', width: 1280, height: 896 },
-  { ratio: '6:5', width: 300, height: 250 },
-  { ratio: '4:15', width: 160, height: 600 },
-  { ratio: '1:2', width: 300, height: 600 },
-  { ratio: '728:90', width: 728, height: 90 },
-  { ratio: '970:90', width: 970, height: 90 },
-  { ratio: '970:250', width: 970, height: 250 },
-  { ratio: '320:100', width: 320, height: 100 },
+  { ratio: '4:3', width: 1280, height: 896 }
 ]
 
 // Random prompt list the user can use if they lack prompt ideas
