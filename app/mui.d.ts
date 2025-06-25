983292973a8fb59beaabaa70fe3dbbd27e4da1a2
brianlau127 <brianlau2002@gmail.com>
2025-06-25 16:40:39 +0800

    Update mui.d.ts

--- app/mui.d.ts
+++ app/mui.d.ts
@@ -1,1 +1,19 @@
+// This file tells TypeScript that we are adding new properties to the MUI theme.
+import { PaletteColorOptions } from '@mui/material/styles';
 
+declare module '@mui/material/styles' {
+  // Allow us to add our custom color to the palette
+  interface PaletteOptions {
+    brand?: PaletteColorOptions;
+  }
+  interface Palette {
+    brand: PaletteColorOptions;
+  }
+}
+
+// Allow us to use our custom color on the Button component
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    brand: true;
+  }
+}
