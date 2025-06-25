5ae1b88935b69f8e0d4392d732df9451f67d578d
brianlau127 <brianlau2002@gmail.com>
2025-06-25 15:27:35 +0800

    Update theme.ts

--- app/theme.ts
+++ app/theme.ts
@@ -18,6 +18,7 @@ import { createTheme } from '@mui/material/styles'
 const theme = createTheme({
   palette: {
     mode: 'light',
+    divider: '#BDBDBD', 
     primary: {
       main: '#4285F4',
       dark: '#1967D2',
